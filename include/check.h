df8ab6319751bd0e8bf8fada03583700b15cddd4
Toiletpaperepic <100048487+Toiletpaperepic@users.noreply.github.com>
2025-07-16 23:36:06 -0400

    add release and debug compile flags and don't show source files on release

--- include/check.h
+++ include/check.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <errno.h>
 
+#if __debug__
 /// don't exit on error
 #define check(expression)   \
     {   \
@@ -21,3 +22,25 @@
             exit(1);    \
         }   \
     }
+#elif __release__
+//hide filenames in release
+
+/// don't exit on error
+#define check(expression)   \
+    {   \
+        int result = expression;    \
+        if (result) {    \
+            printf("Error: got %i, reason: %s\n", result, strerror(errno));  \
+        }   \
+    }
+
+/// exit on error
+#define check2(expression)   \
+    {   \
+        int result = expression;    \
+        if (result) {    \
+            printf("Error: got %i, reason: %s\n", result, strerror(errno));  \
+            exit(1);    \
+        }   \
+    }
+#endif

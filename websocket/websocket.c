d0ca439d5a7c0adb4cdcedd461606ec39dc554b9
Toiletpaperepic <100048487+Toiletpaperepic@users.noreply.github.com>
2025-10-01 16:46:19 -0400

    Revert "support for Fragmentation"
    This reverts commit dc299208259eeb55695820b4421fa5d7bb482144.

--- websocket/websocket.c
+++ websocket/websocket.c
@@ -8,8 +8,6 @@
 #include "websocket.h"
 #include "https_headers.h"
 
-#define PAYLOAD_MAX 125
-
 // https://en.wikipedia.org/wiki/WebSocket#Protocol
 
 #if defined(__ANDROID__) && __ANDROID_API__ < 28
@@ -68,73 +66,68 @@ struct connection websocket_connect(struct parsed_url purl) {
 }
 
 void websocket_send(struct connection con, void* buffer, size_t size) {
-    for (int i = 1; i <= size; i++) {
-        if (i % PAYLOAD_MAX == 0 || i == size) {
-            int payloadsize = ((i - 1) % PAYLOAD_MAX) + 1;
-            uint8_t payload[2 + 4 + payloadsize] = {};
-            
-            printf("Payload size: 2 + 4 + %d = %lu\n", payloadsize, sizeof(payload));
+    uint8_t payload[2 + 4 + size] = {};
+    bool FIN = true;
 
-            bool FIN = i != 0;
-            if(FIN == true) {
-                payload[0] = payload[0] | 0b10000000;
-            }
-        
-            // enum opcodes opcode = TEXT;
-            enum opcodes opcode = BINARY;
-        
-            switch (opcode) {
-                case CONTINUATION:
-                    payload[0] = payload[0] | CONTINUATION;
-                    break;
-                case TEXT:
-                    payload[0] = payload[0] | TEXT;
-                    break;
-                case BINARY:
-                    payload[0] = payload[0] | BINARY;
-                    break;
-                case CLOSE:
-                    payload[0] = payload[0] | CLOSE;
-                    break;
-                case PING:
-                    payload[0] = payload[0] | PING;
-                    break;
-                case PONG:
-                    payload[0] = payload[0] | PONG;
-                    break;
-            }
-        
-            assert((payload[0] & 0b01110000) == 0);
-        
-            // masked is always true as a client.
-            payload[1] = payload[1] | 0b10000000;
-        
-            assert(payloadsize <= PAYLOAD_MAX);
-            payload[1] = payload[1] | payloadsize;
-        
-            uint8_t maskingkey[4] = {};
-            getrandom(&maskingkey, sizeof(maskingkey), 0);
-        
-            printf("masking key: ");
-            for (int i = 0; i < sizeof(maskingkey); i++)
-                printf("%X ", maskingkey[i]);
-            printf("\n");
-        
-            memcpy(payload + 2, &maskingkey, sizeof(maskingkey));
-            memcpy(payload + 6, buffer + (i - payloadsize), payloadsize);
-        
-            printf("payload (masked): ");
-            for (int i = 0; i < payloadsize; i++) {
-                payload[6 + i] = payload[6 + i] ^ maskingkey[i % 4];
-                printf("%X ", payload[6 + i]);
-            }
-            printf("\n");
-        
-            if (send(con.fd, payload, sizeof(payload), 0) < 0) {
-                fprintf(stderr, "send(): %s.\n", strerror(errno));
-                exit(errno);
-            }
-        }
+    if(FIN == true) {
+        payload[0] = payload[0] | 0b10000000;
+    } else {
+        // payload[0] = payload[0] | 0b00000000;
+    }
+
+    // enum opcodes opcode = TEXT;
+    enum opcodes opcode = BINARY;
+
+    switch (opcode) {
+        case CONTINUATION:
+            payload[0] = payload[0] | CONTINUATION;
+            break;
+        case TEXT:
+            payload[0] = payload[0] | TEXT;
+            break;
+        case BINARY:
+            payload[0] = payload[0] | BINARY;
+            break;
+        case CLOSE:
+            payload[0] = payload[0] | CLOSE;
+            break;
+        case PING:
+            payload[0] = payload[0] | PING;
+            break;
+        case PONG:
+            payload[0] = payload[0] | PONG;
+            break;
+    }
+
+    assert((payload[0] & 0b01110000) == 0);
+
+    // masked is always true as a client.
+    payload[1] = payload[1] | 0b10000000;
+
+    assert(size < 127);
+    payload[1] = payload[1] | size;
+
+    uint8_t maskingkey[4] = {};
+    getrandom(&maskingkey, sizeof(maskingkey), 0);
+
+    printf("masking key: ");
+    for (int i = 0; i < sizeof(maskingkey); i++)
+        printf("%X ", maskingkey[i]);
+    printf("\n");
+
+    memcpy(&payload[2], &maskingkey, sizeof(maskingkey));
+    memcpy(&payload[6], buffer, size);
+
+    printf("payload (masked): ");
+    for (int i = 0; i < size; i++) {
+        payload[6 + i] = payload[6 + i] ^ maskingkey[i % 4];
+        printf("%X ", payload[6 + i]);
+    }
+    printf("\n");
+
+    if (send(con.fd, payload, sizeof(payload), 0) < 0) {
+        fprintf(stderr, "send(): %s.\n", strerror(errno));
+        exit(errno);
     }
 }
 
@@ -145,7 +138,7 @@ struct message websocket_recv(struct connection con) {
         exit(errno);
     }
 
-    bool FIN = (header[0] & 0b10000000) != 0;
+    int FIN = (header[0] & 0b10000000) != 0;
     printf("FIN: %i\n", FIN);
     assert(FIN == 1);
 
@@ -155,23 +148,14 @@ struct message websocket_recv(struct connection con) {
     printf("opcode: %i\n", opcode);
     assert(opcode == TEXT || opcode == BINARY);
 
-    bool masked = (header[1] & 0b10000000) != 0;
+    int masked = (header[1] & 0b10000000) != 0;
     printf("masked: %i\n", masked);
     assert(masked == 0);
 
-    uint64_t payload_size = header[1] & 0b01111111;
-    if (payload_size == 126) {
-        if (recv(con.fd, &payload_size, 2, 0) < 0) {
-            fprintf(stderr, "recv(): %s.\n", strerror(errno));
-            exit(errno);
-        }
-    } else if (payload_size == 127) {
-        if (recv(con.fd, &payload_size, 8, 0) < 0) {
-            fprintf(stderr, "recv(): %s.\n", strerror(errno));
-            exit(errno);
-        }
-    }
-    printf("payload size: %lu\n", payload_size);
+    unsigned int payload_size = header[1] & 0b01111111;
+    printf("payload size: %i\n", payload_size);
+
+    memset(header, '\0', sizeof(header));
 
     struct message msg;
     msg.size = payload_size;

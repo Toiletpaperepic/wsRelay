9518458cba5d390f8a48f2fcc39a943fe21df3ed
Toiletpaperepic <100048487+Toiletpaperepic@users.noreply.github.com>
2025-10-02 19:17:57 -0400

    Update websocket/websocket.c

--- websocket/websocket.c
+++ websocket/websocket.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <sys/random.h>
 #include <arpa/inet.h>
 #include <string.h>
@@ -65,14 +66,15 @@ struct connection websocket_connect(struct parsed_url purl) {
     return con;
 }
 
-void websocket_send(struct connection con, void* buffer, size_t size) {
-    uint8_t payload[2 + 4 + size] = {};
+void websocket_send(struct connection con, void* buffer, uint64_t size) {
+    uint8_t byte0 = 0;
+    uint8_t byte1 = 0;
     bool FIN = true;
 
     if(FIN == true) {
-        payload[0] = payload[0] | 0b10000000;
+        byte0 = byte0 | 0b10000000;
     } else {
-        // payload[0] = payload[0] | 0b00000000;
+        // byte0 = byte0 | 0b00000000;
     }
 
     // enum opcodes opcode = TEXT;
@@ -80,32 +82,43 @@ void websocket_send(struct connection con, void* buffer, size_t size) {
 
     switch (opcode) {
         case CONTINUATION:
-            payload[0] = payload[0] | CONTINUATION;
+            byte0 = byte0 | CONTINUATION;
             break;
         case TEXT:
-            payload[0] = payload[0] | TEXT;
+            byte0 = byte0 | TEXT;
             break;
         case BINARY:
-            payload[0] = payload[0] | BINARY;
+            byte0 = byte0 | BINARY;
             break;
         case CLOSE:
-            payload[0] = payload[0] | CLOSE;
+            byte0 = byte0 | CLOSE;
             break;
         case PING:
-            payload[0] = payload[0] | PING;
+            byte0 = byte0 | PING;
             break;
         case PONG:
-            payload[0] = payload[0] | PONG;
+            byte0 = byte0 | PONG;
             break;
     }
 
-    assert((payload[0] & 0b01110000) == 0);
+    assert((byte0 & 0b01110000) == 0);
 
     // masked is always true as a client.
-    payload[1] = payload[1] | 0b10000000;
+    byte1 = byte1 | 0b10000000;
+    unsigned int extraPayloadlength = 0;
 
-    assert(size < 127);
-    payload[1] = payload[1] | size;
+    if (size <= 125) {
+        byte1 = byte1 | size;
+        printf("size is smaller then 125\n");
+    } else if (size >= 125 && size < UINT16_MAX) { // 16 bits
+        byte1 = byte1 | 126;
+        extraPayloadlength += sizeof(uint16_t);
+        printf("size is smaller then UINT16_MAX\n");
+    } else if (size >= 125 && size > UINT16_MAX && size < UINT64_MAX) { // 64 bits
+        byte1 = byte1 | 127;
+        extraPayloadlength += sizeof(uint64_t);
+        printf("size is smaller then UINT64_MAX\n");
+    }
 
     uint8_t maskingkey[4] = {};
     getrandom(&maskingkey, sizeof(maskingkey), 0);
@@ -115,15 +128,23 @@ void websocket_send(struct connection con, void* buffer, size_t size) {
         printf("%X ", maskingkey[i]);
     printf("\n");
 
-    memcpy(&payload[2], &maskingkey, sizeof(maskingkey));
-    memcpy(&payload[6], buffer, size);
+    uint8_t payload[2 + extraPayloadlength + sizeof(maskingkey) + size] = {};
+
+    memcpy(payload, &byte0, sizeof(byte0));
+    memcpy(payload + 1, &byte1, sizeof(byte1));
+    if (extraPayloadlength > 0)
+        memcpy(payload + 2, &size, extraPayloadlength);
+    memcpy(payload + 2 + extraPayloadlength, maskingkey, sizeof(maskingkey));
+    memcpy(payload + 2 + extraPayloadlength + sizeof(maskingkey), buffer, size);
 
     printf("payload (masked): ");
     for (int i = 0; i < size; i++) {
-        payload[6 + i] = payload[6 + i] ^ maskingkey[i % 4];
-        printf("%X ", payload[6 + i]);
+        payload[2 + extraPayloadlength + sizeof(maskingkey) + i] = payload[2 + extraPayloadlength + sizeof(maskingkey) + i] ^ maskingkey[i % 4];
+        printf("%X ", payload[2 + extraPayloadlength + sizeof(maskingkey) + i]);
     }
     printf("\n");
+
+    printf("payload (size):%lu\n", sizeof(payload));
 
     if (send(con.fd, payload, sizeof(payload), 0) < 0) {
         fprintf(stderr, "send(): %s.\n", strerror(errno));

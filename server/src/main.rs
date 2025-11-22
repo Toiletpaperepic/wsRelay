adec82acf258c354aa79920428e60424d35dbd82
Toiletpaperepic <100048487+Toiletpaperepic@users.noreply.github.com>
2025-11-22 13:19:35 -0500

    Update server/src/main.rs

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,12 +11,11 @@ fn index() -> &'static str {
 async fn connect(ws: ws::WebSocket) -> ws::Channel<'static> {
     ws.channel(move |mut stream| Box::pin(async move {
         while let Some(message) = stream.next().await {
-            let message = message?;
-            info!("payload: {}, payload (size): {}, binary: {}, text: {}", message, message.len(), message.is_binary(), message.is_text());
+            let message = message?;   
 
             let opcode;
 
-             if message.is_text() {
+            if message.is_text() {
                 opcode = 1;
             } else {
                 opcode = 2;
@@ -24,12 +23,25 @@ async fn connect(ws: ws::WebSocket) -> ws::Channel<'static> {
 
             // let _ = stream.send(Message::Binary("hello, world!".into())).await;
 
-            if message.to_string() == "exit\n" {
-                let _ = stream.send(Message::Close(None)).await;
-            }
+            if opcode == 1 {
+                info!("payload: {}, payload (size): {}, binary: {}, text: {}", message, message.len(), message.is_binary(), message.is_text());
 
-            let _ = stream.send(Message::Frame(Frame::message(message.to_string()[0..message.len() / 2].into(), opcode.into(), false))).await;
-            let _ = stream.send(Message::Frame(Frame::message(message.to_string()[message.len() / 2..message.len()].into(), opcode.into(), true))).await;
+                if message.to_string() == "exit\n" {
+                    let _ = stream.send(Message::Close(None)).await;
+                }
+
+                let _ = stream.send(Message::Frame(Frame::message(message.clone().into_text()?[0..message.len() / 2].into(), opcode.into(), false))).await;
+                let _ = stream.send(Message::Frame(Frame::message(message.clone().into_text()?[message.len() / 2..message.len()].into(), opcode.into(), true))).await;
+            } else {
+                info!("payload: {:?}, payload (size): {}, binary: {}, text: {}", message.clone().into_data(), message.len(), message.is_binary(), message.is_text());
+
+                if message.clone().into_data() == "exit\n".as_bytes() {
+                    let _ = stream.send(Message::Close(None)).await;
+                }
+
+                let _ = stream.send(Message::Frame(Frame::message(message.clone().into_data()[0..message.len() / 2].into(), opcode.into(), false))).await;
+                let _ = stream.send(Message::Frame(Frame::message(message.clone().into_data()[message.len() / 2..message.len()].into(), opcode.into(), true))).await;
+            }
 
             // ok(());
         }

f187f0abf54521e7ab3200f702d061e2792a514d
Toiletpaperepic <100048487+Toiletpaperepic@users.noreply.github.com>
2026-03-22 12:06:02 -0400

    uhhhhhhhh

--- websocket/dll_export.h
+++ websocket/dll_export.h
@@ -1,7 +1,9 @@
-#if defined(_WIN32) && !defined(DLL_EXPORT)
+#if defined(DLL_EXPORT)
+// already included
+#elif defined(_WIN32)
 // MSVC Windows: DLL export
 #define DLL_EXPORT __declspec(dllexport)
 #else
-// leave it empty if system is on not on windows or already included.
+// leave it empty if system is on not on windows.
 #define DLL_EXPORT 
 #endif

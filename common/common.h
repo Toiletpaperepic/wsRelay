7cd5b756b22f28726a8fb8c17d4a42a3d47facf6
Toiletpaperepic <100048487+Toiletpaperepic@users.noreply.github.com>
2026-03-28 09:51:52 -0400

    lower c standard

--- common/common.h
+++ common/common.h
@@ -52,6 +52,7 @@
 #endif
 
 #if STRING_TO_INT_CONVERSION
+#include <stdbool.h>
 bool strtoint16(int16_t* result, const char* str);
 bool strtouint16(uint16_t* result, const char* str);
 bool strtoint32(int32_t* result, const char* str);
